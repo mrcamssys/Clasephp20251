8c95ab21181b95a3d69a19a3c724054a8b3af597
Carlos Arturo Moreno <39220043+mrcamssys@users.noreply.github.com>
2025-02-27 23:06:39 +0000

    fix: creamos un formulario en contenedor

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
   //import HelloWorld from './components/HelloWorld.vue'
+  import ContenedorEjemplo from './components/contenedor.vue'
 </script>
 
 <template>
+  
   <!--
   <HelloWorld msg="Vite + Vue" />
   -->
+
+  <ContenedorEjemplo></ContenedorEjemplo>
+
 
   
 </template>
